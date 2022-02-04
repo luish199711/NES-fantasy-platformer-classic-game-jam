79ee329c3169a3d84eff3711aa321b14d908793a
Luis Hernandez <53665820+luish199711@users.noreply.github.com>
2022-02-03 20:44:34 -0600

    Border progress onto snake game

--- test.c
+++ test.c
@@ -56,95 +56,67 @@ typedef struct {
   int human:1;
 } Player;
 
+Player players[2];
+
 
 const char BOX_CHARS[8] = { '+','+','+','+','-','-','!','!' };
 
 void draw_box(byte x, byte y, byte x2, byte y2, const char* chars) {
-  byte x1 = x;
-  cputcxy(x, y, chars[2]);
-  cputcxy(x2, y, chars[3]);
-  cputcxy(x, y2, chars[0]);
-  cputcxy(x2, y2, chars[1]);
+  byte x1 = x; //copy x into x1
+
+  //put '+' onto the corners of the screen
+  cputcxy(x, y, chars[2]);//	Top Left
+  cputcxy(x2, y, chars[3]);//	Top Right
+  cputcxy(x, y2, chars[0]);//	Bottom Left
+  cputcxy(x2, y2, chars[1]);//	Bottom Right
+
   while (++x < x2) {
-    cputcxy(x, y, chars[5]);
-    cputcxy(x, y2, chars[4]);
+    cputcxy(x, y, chars[5]);//	Top side
+    cputcxy(x, y2, chars[4]);// Bottom side
   }
   while (++y < y2) {
-    cputcxy(x1, y, chars[6]);
-    cputcxy(x2, y, chars[7]);
+    cputcxy(x1, y, chars[6]);//	Left side
+    cputcxy(x2, y, chars[7]);// Right side
   }
 }
 
 void draw_playfield() {
   draw_box(1,2,COLS-2,ROWS-1,BOX_CHARS);
-//  cputcxy(9,1,players[0].score+'0');	//Player 1 Score
-//  cputcxy(28,1,players[1].score+'0');	//Player 2 Score
+//  cputcxy(28,1,'0');	//Player 2 Score
 //  if (attract) {
-    cputsxy(3,ROWS-1,"ATTRACT MODE - PRESS ENTER");
+//    cputsxy(3,ROWS-1,"ATTRACT MODE - PRESS ENTER");
 //  } else {
 //    cputsxy(1,1,"PLYR1:");
 //    cputsxy(20,1,"PLYR2:");
 //  }
 }
 
-
-
-
-#define AE(tl,tr,bl,br) (((tl)<<0)|((tr)<<2)|((bl)<<4)|((br)<<6))
-
-// this is attribute table data, 
-// each 2 bits defines a color palette
-// for a 16x16 box
-const unsigned char Attrib_Table[0x40]={
-AE(3,3,1,0),AE(3,3,0,0),AE(3,3,0,0),AE(3,3,0,0), AE(2,2,0,0),AE(2,2,0,0),AE(2,2,0,0),AE(2,2,0,1),
-AE(1,0,1,0),AE(0,0,0,0),AE(0,0,0,0),AE(0,0,0,0), AE(0,0,0,0),AE(0,0,0,0),AE(0,0,0,0),AE(0,1,0,1),
-AE(1,0,1,0),AE(0,0,0,0),AE(0,0,0,0),AE(0,0,0,0), AE(0,0,0,0),AE(0,0,0,0),AE(0,0,0,0),AE(0,1,0,1),
-AE(1,0,1,0),AE(0,0,0,0),AE(0,0,0,0),AE(0,0,0,0), AE(0,0,0,0),AE(0,0,0,0),AE(0,0,0,0),AE(0,1,0,1),
-AE(1,0,1,0),AE(0,0,0,0),AE(0,0,0,0),AE(0,0,0,0), AE(0,0,0,0),AE(0,0,0,0),AE(0,0,0,0),AE(0,1,0,1),
-AE(1,0,1,0),AE(0,0,0,0),AE(0,0,0,0),AE(0,0,0,0), AE(0,0,0,0),AE(0,0,0,0),AE(0,0,0,0),AE(0,1,0,1),
-AE(1,0,1,0),AE(0,0,0,0),AE(0,0,0,0),AE(0,0,0,0), AE(0,0,0,0),AE(0,0,0,0),AE(0,0,0,0),AE(0,1,0,1),
-AE(1,1,1,1),AE(1,1,1,1),AE(1,1,1,1),AE(1,1,1,1), AE(1,1,1,1),AE(1,1,1,1),AE(1,1,1,1),AE(1,1,1,1),
-};
-
-/*{pal:"nes",layout:"nes"}*/
-const unsigned char Palette_Table[16]={ 
-  0x00,
-  0x01,0x28,0x31,0x00,
-  0x04,0x24,0x34,0x00,
-  0x09,0x29,0x39,0x00,
-  0x06,0x26,0x36
-};
-
-// put 8x8 grid of palette entries into the PPU
-void setup_attrib_table() {
-  vram_adr(0x23c0);
-  vram_write(Attrib_Table, 0x40);
-}
-
-void setup_palette() {
-  int i;
-  // only set palette entries 0-15 (background only)
-  for (i=0; i<16; i++)
-    pal_col(i, Palette_Table[i] ^ attract);
-}
-
 void main(void)
 {
 //  setup_graphics();
   
+  // set palette colors
+  pal_col(0,0x02);	// set screen to dark blue
+  pal_col(1,0x14);	// fuchsia
+  pal_col(2,0x20);	// grey
+  pal_col(3,0x30);	// white
+  
   vrambuf_clear();
   
   set_vram_update(updbuf);
+  memset(players,0,sizeof(players));
   
 //draw message  
 //  vram_adr(NTADR_A(2,2));
 //  vram_write("HELLO, WORLD!", 12);
 //enable rendering
-  ppu_off();
-  setup_attrib_table();
-  setup_palette();
+//  ppu_off();
+//  setup_attrib_table();
+//  setup_palette();
   clrscr();
   draw_playfield();
+  vrambuf_flush();
+  vrambuf_flush();
 //  ppu_on_all();
 //infinite loop
   while(1) {
